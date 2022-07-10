ee8df7da052645c9e5e31c63098de0b72ade1b63
Ashwin Gur <ashwingur88@gmail.com>
2022-07-10 16:28:01 +1000

    Added basic filter

--- src/components/AllCountries.jsx
+++ src/components/AllCountries.jsx
@@ -1,22 +1,55 @@
 import React, { useState, useEffect } from "react";
 import CountryCard from "./CountryCard";
+import "../css/AllCountries.css";
 
 export default function AllCountries() {
   const [allCountries, setAllCountries] = useState([]);
+  const [filter, setFilter] = useState({ isIndependent: false });
 
-  const countryCards = allCountries.map((country) => (
-    <CountryCard {...country} key={country.cca2} />
-  ));
+  const countryCards = allCountries
+    .filter((country) => {
+      console.log(
+        `country.independent: ${country.independent}, ${filter.isIndependent}`
+      );
+      return country.independent == filter.isIndependent;
+    })
+    .map((country) => <CountryCard {...country} key={country.cca2} />);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((rest) => rest.json())
-      .then((data) => setAllCountries(data));
+      .then((data) => {
+        setAllCountries(
+          data.sort((a, b) => a.name.common.localeCompare(b.name.common))
+        );
+      });
   }, []);
+
+  function handleFilter(event) {
+    const { name, value, type, checked } = event.target;
+    setFilter((prevFilter) => {
+      return {
+        ...prevFilter,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  }
 
   return (
     <div className="all-countries">
-      <h1>All Countries</h1>
+      <h1 className="all-countries--title">All Countries</h1>
+      <h2 className="all-countries--count">
+        Currently showing {countryCards.length} countries
+      </h2>
+      <div className="all-countries-filter">
+        <label htmlFor="isIndependent">Independent</label>
+        <input
+          type="checkbox"
+          id="isIndependent"
+          name="isIndependent"
+          onChange={handleFilter}
+        ></input>
+      </div>
       {countryCards}
     </div>
   );
--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,15 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/CountryCard.css";
 
 export default function CountryCard(props) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  function mouseEnterCard() {
+    setIsExpanded(true);
+  }
+  function mouseLeaveCard() {
+    setIsExpanded(false);
+  }
+
+  let languages = [];
+  for (var key in props.languages) {
+    languages.push(props.languages[key]);
+  }
+
+  let currencies = [];
+  for (var key in props.currencies) {
+    currencies.push(`${props.currencies[key].name} (${key})`);
+  }
+
   return (
-    <div className="country-card">
+    <div
+      className="country-card"
+      onMouseEnter={mouseEnterCard}
+      onMouseLeave={mouseLeaveCard}
+    >
       <h2 className="country-card--name">{props.name.common}</h2>
       <div className="country-card--content">
         <img className="country-card--flag" src={props.flags.svg} />
         <div className="country-card--facts">
-          <p className="country-card--capitals">Capitals: {props.capital}</p>
-          <p className="country-card--region">Region: {props.region}</p>
+          <p className="country-card--capitals">
+            <span className="country-card--fact_title">Capitals:</span>{" "}
+            {props.capital}
+          </p>
+          <p className="country-card--region">
+            <span className="country-card--fact_title">Region:</span>{" "}
+            {props.region}
+          </p>
+          <p className="country-card--region">
+            <span className="country-card--fact_title">Languages:</span>{" "}
+            {languages.join(", ")}
+          </p>
+          <p className="country-card--region">
+            <span className="country-card--fact_title">Population:</span>{" "}
+            {props.population.toLocaleString()}
+          </p>
+          {isExpanded && (
+            <div>
+              <p className="country-card--currencies">
+                <span className="country-card--fact_title">Currencies:</span>{" "}
+                {currencies.join(", ")}
+              </p>
+              <p className="country-card--independent">
+                <span className="country-card--fact_title">Independent:</span>{" "}
+                {props.independent ? "Yes" : "No"}
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </div>
