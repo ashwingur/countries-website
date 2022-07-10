834f46e002b05eb3251f898f0e0e21600aa73abc
Ashwin Gur <ashwingur88@gmail.com>
2022-07-10 14:14:57 +1000

    Created very basic layout and got country cards to display

--- src/components/AllCountries.jsx
+++ src/components/AllCountries.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CountryCard from "./CountryCard";
 
 export default function AllCountries() {
+  const [allCountries, setAllCountries] = useState([]);
+
+  const countryCards = allCountries.map((country) => (
+    <CountryCard {...country} key={country.cca2} />
+  ));
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((rest) => rest.json())
+      .then((data) => setAllCountries(data));
+  }, []);
+
   return (
     <div className="all-countries">
       <h1>All Countries</h1>
+      {countryCards}
     </div>
   );
 }
--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,1 +1,17 @@
 import React from "react";
+import "../css/CountryCard.css";
+
+export default function CountryCard(props) {
+  return (
+    <div className="country-card">
+      <h2 className="country-card--name">{props.name.common}</h2>
+      <div className="country-card--content">
+        <img className="country-card--flag" src={props.flags.svg} />
+        <div className="country-card--facts">
+          <p className="country-card--capitals">Capitals: {props.capital}</p>
+          <p className="country-card--region">Region: {props.region}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
